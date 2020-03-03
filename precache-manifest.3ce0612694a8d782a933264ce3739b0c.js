c624c3295fd2049c028afb98eeda2d096b793ef5
npatta01 <npatta01@gmail.com>
2020-03-03 12:21:05 +0000

    Deploying to gh-pages from  - 01d42cd4597cfd91660130939f2f4987d518d8a4 🚀

---
+++ precache-manifest.3ce0612694a8d782a933264ce3739b0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2641986cac6cb711cf1b22d9053ec95e",
+    "url": "/tutorials-react-pokedex/index.html"
+  },
+  {
+    "revision": "5ac5082e95db60dda20c",
+    "url": "/tutorials-react-pokedex/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5ef1d8d83902afed1b20",
+    "url": "/tutorials-react-pokedex/static/css/main.9f5ad587.chunk.css"
+  },
+  {
+    "revision": "5ac5082e95db60dda20c",
+    "url": "/tutorials-react-pokedex/static/js/2.a3bf5414.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/tutorials-react-pokedex/static/js/2.a3bf5414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef1d8d83902afed1b20",
+    "url": "/tutorials-react-pokedex/static/js/main.2993725a.chunk.js"
+  },
+  {
+    "revision": "facbd9488c9a51755c79",
+    "url": "/tutorials-react-pokedex/static/js/runtime-main.9db176b8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tutorials-react-pokedex/precache-manifest.405976f97e5a780ad4436c1eef5d004b.js"
+  "/tutorials-react-pokedex/precache-manifest.3ce0612694a8d782a933264ce3739b0c.js"
 );
 
 self.addEventListener('message', (event) => {
